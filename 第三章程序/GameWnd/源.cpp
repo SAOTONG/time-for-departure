85a7bd48db1527e434b422a44c9673b2e48a7e52
SAO <1053001374@qq.com>
2019-07-31 23:34:37 +0800

    游戏窗口框架

--- 第三章程序/GameWnd/源.cpp
+++ 第三章程序/GameWnd/源.cpp
@@ -9,7 +9,6 @@
 // 描述：包含程序依赖的头文件
 //---------------------------------------------------------------------------
 #include <windows.h>
-#pragma comment(lib,"winmm.lib")
 
 //---------------------------------------------------------------------------
 // 描述：定义一些辅助宏
@@ -21,12 +20,12 @@
 //---------------------------------------------------------------------------
 // 描述：全局函数声明
 //---------------------------------------------------------------------------
-LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
 //---------------------------------------------------------------------------
 // 描述：全局函数声明
 //---------------------------------------------------------------------------
-int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine, int nCmdShow)
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	//设计一个完整的窗口类
 	WNDCLASSEX wndClass = { 0 };
@@ -36,14 +35,60 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 	wndClass.cbClsExtra = 0;//窗口类附加内存
 	wndClass.cbWndExtra = 0;//窗口附加内存
 	wndClass.hInstance = hInstance;//指定包含窗口过程的程序的实例句柄
-	wndClass.hIcon = (HICON)::LoadImage(NULL, L"icon.ico", IMAGE_ICON, 0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);//
+	wndClass.hIcon = (HICON)::LoadImage(NULL, L"cio.ico", IMAGE_ICON,
+		0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
 	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
 	wndClass.lpszMenuName = NULL;
 	wndClass.lpszClassName = L"GameWindow";
+
+	//注册窗口类
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+
+	//正式创建窗口
+	HWND hwnd = CreateWindow(L"GameWindow", WINDOW_TITLE, WS_OVERLAPPEDWINDOW, 
+		CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL,hInstance,NULL);
+
+	//窗口的移动、显示与更新
+	MoveWindow(hwnd, 250, 80, WINDOW_WIDTH, WINDOW_HEIGHT, true);//调整窗口显示位置
+	ShowWindow(hwnd, nCmdShow);//显示窗口
+	UpdateWindow(hwnd);//更新窗口
+
+	//消息循环过程
+	MSG msg = { 0 };//定义并初始化msg
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg,0,0,0,PM_REMOVE))//查看应用程序消息队列，派发消息
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	//窗口类的注销
+	UnregisterClass(L"GameWindow", wndClass.hInstance);
+	return 0;
 }
 
-LRESULT CALLBACK WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	return LRESULT();
+	switch (uMsg)
+	{
+	case WM_PAINT:
+		ValidateRect(hWnd, NULL);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(hWnd);
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd,uMsg,wParam,lParam);
+	}
+	return 0;
 }
