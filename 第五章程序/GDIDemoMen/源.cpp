abd81d65d16a90b7acb7d83218b1966bb8d284d3
SAO <1053001374@qq.com>
2019-08-08 15:18:34 +0800

    前进

---
+++ 第五章程序/GDIDemoMen/源.cpp
@@ -0,0 +1,166 @@
+//---------------------------------------------------------------------------
+// 程序名称：GameWnd
+// 2019年7月 Create by 侯金成
+// 描述：游戏程序框架
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+// 描述：包含程序依赖的头文件
+//---------------------------------------------------------------------------
+#include <windows.h>
+
+//---------------------------------------------------------------------------
+// 描述：定义一些辅助宏
+//---------------------------------------------------------------------------
+#define WINDOW_WIDTH 932
+#define WINDOW_HEIGHT 700
+#define WINDOW_TITLE L"游戏程序框架"
+
+//---------------------------------------------------------------------------
+// 描述：全局变量的声明
+//---------------------------------------------------------------------------
+HDC g_hdc = NULL, g_mdc = NULL;//全局设备环境句柄
+HBITMAP g_hBackground = NULL;
+HBITMAP g_hMan1 = NULL, g_hMan2 = NULL;
+//---------------------------------------------------------------------------
+// 描述：全局函数声明
+//---------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL Game_Init(HWND hWnd);//资源初始化
+VOID Game_Paint(HWND hWnd);//进行绘图
+BOOL Game_CleanUp(HWND hWnd);//资源清理
+
+//---------------------------------------------------------------------------
+// 描述：主函数定义
+//---------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//设计一个完整的窗口类
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX);//设置结构体的字节数
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;//设置窗口的样式
+	wndClass.lpfnWndProc = WndProc;//指向回调函数的远指针
+	wndClass.cbClsExtra = 0;//窗口类附加内存
+	wndClass.cbWndExtra = 0;//窗口附加内存
+	wndClass.hInstance = hInstance;//指定包含窗口过程的程序的实例句柄
+	wndClass.hIcon = (HICON)::LoadImage(NULL, L"icon.ico", IMAGE_ICON,
+		0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = L"GameWindow";
+
+	//注册窗口类
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+
+	//正式创建窗口
+	HWND hWnd = CreateWindow(L"GameWindow", WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
+
+	//窗口的移动、显示与更新
+	MoveWindow(hWnd, 250, 80, WINDOW_WIDTH, WINDOW_HEIGHT, true);//调整窗口显示位置
+	ShowWindow(hWnd, nCmdShow);//显示窗口
+	UpdateWindow(hWnd);//更新窗口
+
+					   //资源初始化，若初始失败，则返回false
+	if (!Game_Init(hWnd))
+	{
+		MessageBox(hWnd, L"资源初始化失败", L"消息窗口", 0);
+		return false;
+	}
+
+	//窗口初始化后，播放音乐
+	//PlaySound(L"Dia Frampton - Walk Away.flac", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+
+	//消息循环过程
+	MSG msg = { 0 };//定义并初始化msg
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))//查看应用程序消息队列，派发消息
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	//窗口类的注销
+	UnregisterClass(L"GameWindow", wndClass.hInstance);
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+// 描述：窗口过程函数WndProc，对窗口消息进行处理
+//---------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT paintStruct;//声明一个PAINTSTRUCT结构体来记录一些绘图信息
+	switch (nMsg)
+	{
+	case WM_PAINT:
+		g_hdc = BeginPaint(hWnd, &paintStruct);//指定窗口进行绘图操作的准备，用与绘图有关的信息填充到paintStruct
+		Game_Paint(hWnd);
+		EndPaint(hWnd, &paintStruct);//标记窗口绘图的结束
+		ValidateRect(hWnd, NULL);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(hWnd);
+		break;
+	case WM_DESTROY:
+		Game_CleanUp(hWnd);//调用自定义的资源清理函数，在窗口退出前对资源进行清理
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, nMsg, wParam, lParam);
+	}
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+// 描述：资源初始化函数Game_Init，进行一些简单的初始化
+//---------------------------------------------------------------------------
+BOOL Game_Init(HWND hWnd)
+{
+	g_hdc = GetDC(hWnd);//获取设备环境句柄
+	g_hBackground = (HBITMAP)LoadImage(NULL, L"bg.bmp", IMAGE_BITMAP, WINDOW_WIDTH, WINDOW_HEIGHT, LR_LOADFROMFILE);
+	g_hMan1 = (HBITMAP)LoadImage(NULL, L"character1.bmp", IMAGE_BITMAP, 640, 579, LR_LOADFROMFILE);
+	g_hMan2 = (HBITMAP)LoadImage(NULL, L"character2.bmp", IMAGE_BITMAP, 800, 584, LR_LOADFROMFILE);	
+	g_mdc = CreateCompatibleDC(g_hdc);
+
+	Game_Paint(hWnd);//执行绘制操作
+	ReleaseDC(hWnd, g_hdc);//释放设备环境句柄
+	return true;
+}
+
+//---------------------------------------------------------------------------
+// 描述：绘制函数Game_Paint，进行绘制操作
+//---------------------------------------------------------------------------
+VOID Game_Paint(HWND hWnd)
+{
+	SelectObject(g_mdc, g_hBackground);
+	BitBlt(g_hdc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, g_mdc, 0, 0, SRCCOPY);
+
+	SelectObject(g_mdc, g_hMan1);
+	BitBlt(g_hdc, 50, WINDOW_HEIGHT - 579, 320, 579, g_mdc, 320, 0, SRCAND);
+	BitBlt(g_hdc, 50, WINDOW_HEIGHT - 579, 320, 579, g_mdc, 0, 0, SRCPAINT);
+
+	SelectObject(g_mdc, g_hMan2);
+	BitBlt(g_hdc, 450, WINDOW_HEIGHT - 584, 400, 584, g_mdc, 400, 0, SRCAND);
+	BitBlt(g_hdc, 450, WINDOW_HEIGHT - 584, 400, 584, g_mdc, 0, 0, SRCPAINT);
+}
+
+//---------------------------------------------------------------------------
+// 描述：资源清理Game_CleanUp，进行退出前资源的清理工作
+//---------------------------------------------------------------------------
+BOOL Game_CleanUp(HWND hWnd)
+{
+	DeleteObject(g_hBackground);
+	DeleteObject(g_hMan1);
+	DeleteObject(g_hMan2);
+	DeleteDC(g_mdc);
+	return true;
+}
---
+++ 第六章程序/GDIDemo1/源.cpp
@@ -0,0 +1,182 @@
+//---------------------------------------------------------------------------
+// 程序名称：GameWnd
+// 2019年7月 Create by 侯金成
+// 描述：游戏程序框架
+//---------------------------------------------------------------------------
+
+
+//---------------------------------------------------------------------------
+// 描述：包含程序依赖的头文件
+//---------------------------------------------------------------------------
+#include <windows.h>
+#include <tchar.h>
+#pragma comment(lib,"winmm.lib")
+//---------------------------------------------------------------------------
+// 描述：定义一些辅助宏
+//---------------------------------------------------------------------------
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+#define WINDOW_TITLE L"游戏程序框架"
+
+//---------------------------------------------------------------------------
+// 描述：全局变量的声明
+//---------------------------------------------------------------------------
+HDC g_hdc = NULL, g_mdc = NULL;//全局设备环境句柄
+HBITMAP g_hGirl[11];//声明位图数组，用来储存人物位图
+DWORD g_tPre = 0,g_tNow = 0;//声明两个变量记录时间，前者是上一次绘图时间，后者是本次准备绘图的当前时间
+int g_iNum = 0;//记录目前显示的图号
+
+//---------------------------------------------------------------------------
+// 描述：全局函数声明
+//---------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+BOOL Game_Init(HWND hWnd);//资源初始化
+VOID Game_Paint(HWND hWnd);//进行绘图
+BOOL Game_CleanUp(HWND hWnd);//资源清理
+
+							 //---------------------------------------------------------------------------
+							 // 描述：主函数定义
+							 //---------------------------------------------------------------------------
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	//设计一个完整的窗口类
+	WNDCLASSEX wndClass = { 0 };
+	wndClass.cbSize = sizeof(WNDCLASSEX);//设置结构体的字节数
+	wndClass.style = CS_HREDRAW | CS_VREDRAW;//设置窗口的样式
+	wndClass.lpfnWndProc = WndProc;//指向回调函数的远指针
+	wndClass.cbClsExtra = 0;//窗口类附加内存
+	wndClass.cbWndExtra = 0;//窗口附加内存
+	wndClass.hInstance = hInstance;//指定包含窗口过程的程序的实例句柄
+	wndClass.hIcon = (HICON)::LoadImage(NULL, L"icon.ico", IMAGE_ICON,
+		0, 0, LR_DEFAULTSIZE | LR_LOADFROMFILE);
+	wndClass.hCursor = LoadCursor(NULL, IDC_ARROW);
+	wndClass.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
+	wndClass.lpszMenuName = NULL;
+	wndClass.lpszClassName = L"GameWindow";
+
+	//注册窗口类
+	if (!RegisterClassEx(&wndClass))
+	{
+		return -1;
+	}
+
+	//正式创建窗口
+	HWND hWnd = CreateWindow(L"GameWindow", WINDOW_TITLE, WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, WINDOW_WIDTH, WINDOW_HEIGHT, NULL, NULL, hInstance, NULL);
+
+	//窗口的移动、显示与更新
+	MoveWindow(hWnd, 250, 80, WINDOW_WIDTH, WINDOW_HEIGHT, true);//调整窗口显示位置
+	ShowWindow(hWnd, nCmdShow);//显示窗口
+	UpdateWindow(hWnd);//更新窗口
+
+					   //资源初始化，若初始失败，则返回false
+	if (!Game_Init(hWnd))
+	{
+		MessageBox(hWnd, L"资源初始化失败", L"消息窗口", 0);
+		return false;
+	}
+
+	//窗口初始化后，播放音乐
+	//PlaySound(L"Dia Frampton - Walk Away.flac", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP);
+
+	//消息循环过程
+	MSG msg = { 0 };//定义并初始化msg
+	while (msg.message != WM_QUIT)
+	{
+		if (PeekMessage(&msg, 0, 0, 0, PM_REMOVE))//查看应用程序消息队列，派发消息
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else
+		{
+			g_tNow = GetTickCount();
+			if (g_tNow - g_tPre >= 80)//上次绘图时间与当前绘图时间间隔80毫秒的话就绘图
+			{
+				Game_Paint(hWnd);
+			}
+		}
+	}
+
+	//窗口类的注销
+	UnregisterClass(L"GameWindow", wndClass.hInstance);
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+// 描述：窗口过程函数WndProc，对窗口消息进行处理
+//---------------------------------------------------------------------------
+LRESULT CALLBACK WndProc(HWND hWnd, UINT nMsg, WPARAM wParam, LPARAM lParam)
+{
+	PAINTSTRUCT paintStruct;//声明一个PAINTSTRUCT结构体来记录一些绘图信息
+	switch (nMsg)
+	{
+	case WM_PAINT:
+		g_hdc = BeginPaint(hWnd, &paintStruct);//指定窗口进行绘图操作的准备，用与绘图有关的信息填充到paintStruct
+		Game_Paint(hWnd);
+		EndPaint(hWnd, &paintStruct);//标记窗口绘图的结束
+		ValidateRect(hWnd, NULL);
+		break;
+	case WM_KEYDOWN:
+		if (wParam == VK_ESCAPE)
+			DestroyWindow(hWnd);
+		break;
+	case WM_DESTROY:
+		Game_CleanUp(hWnd);//调用自定义的资源清理函数，在窗口退出前对资源进行清理
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hWnd, nMsg, wParam, lParam);
+	}
+	return 0;
+}
+
+//---------------------------------------------------------------------------
+// 描述：资源初始化函数Game_Init，进行一些简单的初始化
+//---------------------------------------------------------------------------
+BOOL Game_Init(HWND hWnd)
+{
+	g_hdc = GetDC(hWnd);//获取设备环境句柄
+	wchar_t filename[20];//用于存放文件名称的字符数组
+	for (int i = 0; i < 11; i++)
+	{
+		memset(filename, 0, sizeof(filename));//初始化字符数组
+		swprintf_s(filename, L"%d.bmp", i);//得到对应图像名称
+		g_hGirl[i] = (HBITMAP)LoadImage(NULL, filename, IMAGE_BITMAP, 
+			WINDOW_WIDTH, WINDOW_HEIGHT, LR_LOADFROMFILE);//加载该图像
+	}
+	g_mdc = CreateCompatibleDC(g_hdc);//建立兼容设备环境的内存DC
+
+	Game_Paint(hWnd);//执行绘制操作
+	
+	return true;
+}
+
+//---------------------------------------------------------------------------
+// 描述：绘制函数Game_Paint，进行绘制操作
+//---------------------------------------------------------------------------
+VOID Game_Paint(HWND hWnd)
+{
+	if (g_iNum ==11)
+	{
+		g_iNum = 0;
+	}
+	SelectObject(g_mdc, g_hGirl[g_iNum]);
+	BitBlt(g_hdc, 0, 0, WINDOW_WIDTH, WINDOW_HEIGHT, g_mdc, 0, 0, SRCCOPY);
+	g_tPre = GetTickCount();
+	g_iNum++;
+}
+
+//---------------------------------------------------------------------------
+// 描述：资源清理Game_CleanUp，进行退出前资源的清理工作
+//---------------------------------------------------------------------------
+BOOL Game_CleanUp(HWND hWnd)
+{
+	for (int i = 0; i < 11; i++)
+	{
+		DeleteObject(g_hGirl[i]);
+	}
+	DeleteDC(g_mdc);
+	ReleaseDC(hWnd, g_hdc);//释放设备环境句柄
+	return true;
+}
