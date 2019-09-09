835745de27d80aa031ef2992aa98a836884fc481
SAO <1053001374@qq.com>
2019-09-09 16:24:16 +0800

    sss

--- 第14章程序/D3Ddemo/源.cpp
+++ 第14章程序/D3Ddemo/源.cpp
@@ -26,17 +26,6 @@
 #define WINDOW_TITLE	L"【致我们永不熄灭的游戏开发梦想】Direct3D顶点缓存的逆袭 示例程序"	//为窗口标题定义的宏
 #define SAFE_RELEASE(p) { if(p) { (p)->Release(); (p)=NULL; } }   //定义一个安全释放宏，便于后面COM接口指针的释放
 
-
-//------------------------------------------------------------------------------------------------
-// 【顶点缓存使用四步曲之一】：设计顶点格式
-//------------------------------------------------------------------------------------------------
-struct CUSTOMVERTEX
-{
-	FLOAT x, y, z;
-	DWORD color;
-};
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ|D3DFVF_DIFFUSE)  //FVF灵活顶点格式
-
 //-----------------------------------【全局变量声明部分】-------------------------------------
 //	描述：全局变量的声明
 //------------------------------------------------------------------------------------------------
@@ -44,9 +33,11 @@ LPDIRECT3DDEVICE9					g_pd3dDevice = NULL; //Direct3D设备对象
 ID3DXFont*								g_pFont = NULL;    //字体COM接口
 float											g_FPS = 0.0f;       //一个浮点型的变量，代表帧速率
 wchar_t										g_strFPS[50];    //包含帧速率的字符数组
-LPDIRECT3DVERTEXBUFFER9		g_pVertexBuffer = NULL;    //顶点缓冲区对象
-LPDIRECT3DINDEXBUFFER9 g_pIndexBuffer = NULL;//索引缓存对象
-
+LPD3DXMESH      g_teapot = NULL;//茶壶对象
+LPD3DXMESH      g_cube = NULL;//立方体对象
+LPD3DXMESH      g_sphere = NULL;//球面体对象
+LPD3DXMESH      g_torus = NULL;//圆环体对象
+D3DXMATRIX      g_WorldMatrix[4], R;//定义全局的世界矩阵
 
 //-----------------------------------【全局函数声明部分】-------------------------------------
 //	描述：全局函数声明，防止“未声明的标识”系列错误
@@ -59,9 +50,9 @@ VOID							Direct3D_CleanUp();				//在这个函数中清理COM资源以及其他资源
 float							Get_FPS();
 VOID                            Matrix_Set();       //四大变换的封装函数
 
-//-----------------------------------【WinMain( )函数】--------------------------------------
-//	描述：Windows应用程序的入口函数，我们的程序从这里开始
-//------------------------------------------------------------------------------------------------
+													//-----------------------------------【WinMain( )函数】--------------------------------------
+													//	描述：Windows应用程序的入口函数，我们的程序从这里开始
+													//------------------------------------------------------------------------------------------------
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
 {
 	//【1】窗口创建四步曲之一：开始设计一个完整的窗口类
@@ -78,7 +69,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	wndClass.lpszMenuName = NULL;						//用一个以空终止的字符串，指定菜单资源的名字。
 	wndClass.lpszClassName = L"ForTheDreamOfGameDevelop";		//用一个以空终止的字符串，指定窗口类的名字。
 
-	//【2】窗口创建四步曲之二：注册窗口类
+																//【2】窗口创建四步曲之二：注册窗口类
 	if (!RegisterClassEx(&wndClass))				//设计完窗口后，需要对窗口类进行注册，这样才能创建该类型的窗口
 		return -1;
 
@@ -98,9 +89,9 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 	ShowWindow(hwnd, nShowCmd);    //调用ShowWindow函数来显示窗口
 	UpdateWindow(hwnd);						//对窗口进行更新，就像我们买了新房子要装修一样
 
-	//PlaySound(L"神秘.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP); //循环播放背景音乐 
+											//PlaySound(L"神秘.wav", NULL, SND_FILENAME | SND_ASYNC | SND_LOOP); //循环播放背景音乐 
 
-	//【5】消息循环过程
+											//【5】消息循环过程
 	MSG msg = { 0 };  //初始化msg
 	while (msg.message != WM_QUIT)			//使用while循环
 	{
@@ -173,9 +164,9 @@ HRESULT Direct3D_Init(HWND hwnd)
 	else
 		vp = D3DCREATE_SOFTWARE_VERTEXPROCESSING; //不支持硬件顶点运算，无奈只好采用软件顶点运算
 
-	//--------------------------------------------------------------------------------------
-	// 【Direct3D初始化四步曲之三，填内容】：填充D3DPRESENT_PARAMETERS结构体
-	//--------------------------------------------------------------------------------------
+												  //--------------------------------------------------------------------------------------
+												  // 【Direct3D初始化四步曲之三，填内容】：填充D3DPRESENT_PARAMETERS结构体
+												  //--------------------------------------------------------------------------------------
 	D3DPRESENT_PARAMETERS d3dpp;
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
 	d3dpp.BackBufferWidth = WINDOW_WIDTH;
@@ -220,70 +211,21 @@ HRESULT Objects_Init(HWND hwnd)
 		OUT_DEFAULT_PRECIS, DEFAULT_QUALITY, 0, _T("微软雅黑"), &g_pFont)))
 		return E_FAIL;
 	srand(timeGetTime());      //用系统时间初始化随机种子 
-
-	//--------------------------------------------------------------------------------------
-	// 【顶点缓存使用四步曲之二】：创建顶点缓存和索引缓存
-	//--------------------------------------------------------------------------------------
-	//创建顶点缓存
-	if (FAILED(g_pd3dDevice->CreateVertexBuffer(8 * sizeof(CUSTOMVERTEX),
-		0, D3DFVF_CUSTOMVERTEX,
-		D3DPOOL_DEFAULT, &g_pVertexBuffer, NULL)))
-	{
-		return E_FAIL;
-	}
-	//创建索引缓存
-	if (FAILED(g_pd3dDevice->CreateIndexBuffer(36 * sizeof(WORD),
-		0, D3DFMT_INDEX16,
-		D3DPOOL_DEFAULT, &g_pIndexBuffer, NULL)))
-	{
-		return E_FAIL;
-	}
-	//--------------------------------------------------------------------------------------
-	// 【顶点缓存使用四步曲之三】：访问顶点缓存和索引缓存
-	//--------------------------------------------------------------------------------------
-	//顶点数据的设置
-	CUSTOMVERTEX vertices[] =
-	{
-		{-20.0f,20.0f,-20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{-20.0f,20.0f,20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{20.0f,20.0f,20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{20.0f,20.0f,-20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{-20.0f,-20.0f,-20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{-20.0f,-20.0f,20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{20.0f,-20.0f,20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)},
-		{20.0f,-20.0f,-20.0f,D3DCOLOR_XRGB(rand() % 256,rand() % 256,rand() % 256)}
-	};
-	//填充顶点缓冲区
-	VOID* pVertices;
-	if (FAILED(g_pVertexBuffer->Lock(0, sizeof(vertices), (void**)&pVertices, 0)))
-		return E_FAIL;
-	memcpy(pVertices, vertices, sizeof(vertices));
-	g_pVertexBuffer->Unlock();
-
-	//填充索引数据
-	WORD *pIndices = NULL;
-	g_pIndexBuffer->Lock(0, 0, (VOID**)&pIndices, 0);
-	// 顶面S
-	pIndices[0] = 0, pIndices[1] = 1, pIndices[2] = 2;
-	pIndices[3] = 0, pIndices[4] = 2, pIndices[5] = 3;
-	// 正面
-	pIndices[6] = 0, pIndices[7] = 3, pIndices[8] = 7;
-	pIndices[9] = 0, pIndices[10] = 7, pIndices[11] = 4;
-	// 左侧面
-	pIndices[12] = 0, pIndices[13] = 4, pIndices[14] = 5;
-	pIndices[15] = 0, pIndices[16] = 5, pIndices[17] = 1;
-	// 右侧面
-	pIndices[18] = 2, pIndices[19] = 6, pIndices[20] = 7;
-	pIndices[21] = 2, pIndices[22] = 7, pIndices[23] = 3;
-	// 背面
-	pIndices[24] = 2, pIndices[25] = 1, pIndices[26] = 5;
-	pIndices[27] = 2, pIndices[28] = 5, pIndices[29] = 6;
-	// 底面
-	pIndices[30] = 4, pIndices[31] = 6, pIndices[32] = 5;
-	pIndices[33] = 4, pIndices[34] = 7, pIndices[35] = 6;
-
-	g_pIndexBuffer->Unlock();
-
+	
+	//物体的创建
+	if (FAILED(D3DXCreateBox(g_pd3dDevice, 2, 2, 2, &g_cube, NULL)))//创建立方体
+		return false;
+	if (FAILED(D3DXCreateTeapot(g_pd3dDevice, &g_teapot, NULL)))//创建茶壶
+		return false;
+	if (FAILED(D3DXCreateSphere(g_pd3dDevice, 1.5f, 25, 25, &g_sphere, NULL)))//创建球面体
+		return false;
+	if (FAILED(D3DXCreateTorus(g_pd3dDevice, 0.5f, 1.2f, 25, 25, &g_torus,NULL)))//创建圆环体	
+	    return false;
+	
+	//设置渲染状态
+	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);//关闭光照
+	g_pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_CCW);//开启背面消隐
+	g_pd3dDevice->SetRenderState(D3DRS_FILLMODE, D3DFILL_WIREFRAME);//设置线框填充模式
 	return S_OK;
 }
 
@@ -296,7 +238,7 @@ void Direct3D_Render(HWND hwnd)
 	//--------------------------------------------------------------------------------------
 	// 【Direct3D渲染五步曲之一】：清屏操作
 	//--------------------------------------------------------------------------------------
-	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET|D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(255, 214, 158), 1.0f, 0);
+	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER, D3DCOLOR_XRGB(0, 0, 0), 1.0f, 0);
 
 	//定义一个矩形，用于获取主窗口矩形
 	RECT formatRect;
@@ -319,11 +261,27 @@ void Direct3D_Render(HWND hwnd)
 	//------------------------------------------------------------
 	// 【顶点缓存使用四步曲之四】：绘制图形
 	//------------------------------------------------------------
-	g_pd3dDevice->SetStreamSource(0, g_pVertexBuffer, 0, sizeof(CUSTOMVERTEX));
-	g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-	g_pd3dDevice->SetIndices(g_pIndexBuffer);
-	g_pd3dDevice->DrawIndexedPrimitive(D3DPT_TRIANGLELIST, 0, 0, 8, 0, 12);//利用索引缓存配合定点缓存绘制挺行
-
+	D3DXMatrixRotationY(&R, ::timeGetTime() / 1440.0f);//设置公转矩阵
+	//进行立方体的绘制
+	D3DXMatrixTranslation(&g_WorldMatrix[0], 3.0f, -3.0f, 0.0f);
+	g_WorldMatrix[0] = g_WorldMatrix[0] * R;
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &g_WorldMatrix[0]);
+	g_cube->DrawSubset(0);
+	//进行茶壶的绘制
+	D3DXMatrixTranslation(&g_WorldMatrix[1], -3.0f, -3.0f, 0.0f);
+	g_WorldMatrix[1] = g_WorldMatrix[1] * R;
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &g_WorldMatrix[1]);
+	g_teapot->DrawSubset(0);
+	//进行球面体的绘制
+	D3DXMatrixTranslation(&g_WorldMatrix[2], -3.0f, 3.0f, 0.0f);
+	g_WorldMatrix[2] = g_WorldMatrix[2] * R;
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &g_WorldMatrix[2]);
+	g_sphere->DrawSubset(0);
+	//进行圆环体的绘制
+	D3DXMatrixTranslation(&g_WorldMatrix[3], 3.0f, 3.0f, 0.0f);
+	g_WorldMatrix[3] = g_WorldMatrix[3] * R;
+	g_pd3dDevice->SetTransform(D3DTS_WORLD, &g_WorldMatrix[3]);
+	g_torus->DrawSubset(0);
 	//在窗口右上角处，显示每秒帧数
 	int charCount = swprintf_s(g_strFPS, 20, _T("FPS:%0.3f"), Get_FPS());
 	g_pFont->DrawText(NULL, g_strFPS, charCount, &formatRect, DT_TOP | DT_RIGHT, D3DCOLOR_XRGB(255, 39, 136));
@@ -332,9 +290,9 @@ void Direct3D_Render(HWND hwnd)
 	// 【Direct3D渲染五步曲之四】：结束绘制
 	//--------------------------------------------------------------------------------------
 	g_pd3dDevice->EndScene();                       // 结束绘制
-	//--------------------------------------------------------------------------------------
-	// 【Direct3D渲染五步曲之五】：显示翻转
-	//--------------------------------------------------------------------------------------
+													//--------------------------------------------------------------------------------------
+													// 【Direct3D渲染五步曲之五】：显示翻转
+													//--------------------------------------------------------------------------------------
 	g_pd3dDevice->Present(NULL, NULL, NULL, NULL);  // 翻转与显示
 }
 
@@ -352,7 +310,7 @@ float Get_FPS()
 	frameCount++;//每调用一次Get_FPS()函数，帧数自增1
 	currentTime = timeGetTime()*0.001f;//获取系统时间，其中timeGetTime函数返回的是以毫秒为单位的系统时间，所以需要乘以0.001，得到单位为秒的时间
 
-	//如果当前时间减去持续时间大于了1秒钟，就进行一次FPS的计算和持续时间的更新，并将帧数值清零
+									   //如果当前时间减去持续时间大于了1秒钟，就进行一次FPS的计算和持续时间的更新，并将帧数值清零
 	if (currentTime - lastTime > 1.0f) //将时间控制在1秒钟
 	{
 		fps = (float)frameCount / (currentTime - lastTime);//计算这1秒钟的FPS值
@@ -371,22 +329,16 @@ VOID Matrix_Set()
 	//--------------------------------------------------------------------------------------
 	//【四大变换之一】：世界变换矩阵的设置
 	//--------------------------------------------------------------------------------------
-	D3DXMATRIX matWorld, Rx, Ry, Rz;
-	D3DXMatrixIdentity(&matWorld);
-	D3DXMatrixRotationX(&Rx, D3DX_PI*(::timeGetTime() / 1000.0f));
-	D3DXMatrixRotationY(&Ry, D3DX_PI*(::timeGetTime() / 1000.0f / 2));
-	D3DXMatrixRotationZ(&Rz, D3DX_PI*(::timeGetTime() / 1000.0f / 3));
-	matWorld = matWorld * Rx*Ry*Rz;
-	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
+	
 
 	//--------------------------------------------------------------------------------------
 	//【四大变换之一】：取景变换矩阵的设置
 	//--------------------------------------------------------------------------------------
 	D3DXMATRIX matView;
-	D3DXVECTOR3 vEye(0.0f, 0.0f, -200.0f);
-	D3DXVECTOR3 vAt(0.0f, 0.0f, 0.0f);
-	D3DXVECTOR3 vUp(0.0f, 1.0f, 0.0f);
-	D3DXMatrixLookAtLH(&matView, &vEye, &vAt, &vUp);
+	D3DXVECTOR3 vEye(0.0f, 0.0f, -15.0f);//摄像机位置
+	D3DXVECTOR3 vAt(0.0f, 0.0f, 0.0f);//观察点位置
+	D3DXVECTOR3 vUp(0.0f, 1.0f, 0.0f);//向上的向量
+	D3DXMatrixLookAtLH(&matView, &vEye, &vAt, &vUp);//计算取景变换矩阵
 	g_pd3dDevice->SetTransform(D3DTS_VIEW, &matView);
 
 	//--------------------------------------------------------------------------------------
@@ -407,7 +359,6 @@ VOID Matrix_Set()
 	vp.MaxZ = 0.0f;
 	vp.MinZ = 1.0f;
 	g_pd3dDevice->SetViewport(&vp);
-	return VOID();
 }
 
 //-----------------------------------【Direct3D_CleanUp( )函数】--------------------------------
@@ -416,9 +367,12 @@ VOID Matrix_Set()
 void Direct3D_CleanUp()
 {
 	//释放COM接口对象
-	SAFE_RELEASE(g_pVertexBuffer)
-		SAFE_RELEASE(g_pFont)
-		SAFE_RELEASE(g_pd3dDevice)
-		SAFE_RELEASE(g_pIndexBuffer)
+	SAFE_RELEASE(g_pFont)
+	SAFE_RELEASE(g_pd3dDevice)
+		SAFE_RELEASE(g_cube)
+		SAFE_RELEASE(g_sphere)
+		SAFE_RELEASE(g_teapot)
+		SAFE_RELEASE(g_torus)
+		
 }
 
